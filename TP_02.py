a975d48a7c32db6d2921de1323127f39678933b4
BENALI-DZ <com.benali@gmail.com>
2025-10-14 09:04:30 +0100

    Create TP_02.py

---
+++ TP_02.py
@@ -0,0 +1,27 @@
+import cv2
+import matplotlib.pyplot as plt
+
+import numpy as np
+from tensorflow.keras.datasets import mnist
+ 
+
+
+img = cv2.imread('images/car01.jpg', cv2.IMREAD_GRAYSCALE)
+print("Image shape:", img.shape)
+plt.imshow(img, cmap='gray')
+plt.title("Grayscale Image")
+plt.show()
+flattened = img.flatten()
+print("Flattened shape:", flattened.shape)
+normalized = img.astype('float32') / 255.0
+plt.hist(normalized.ravel(), bins=50)
+plt.title("Normalized Pixel Intensity Distribution")
+plt.show()
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+plt.figure(figsize=(5,5))
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(x_train[i], cmap='gray')
+    plt.axis('off')
+    plt.show()
